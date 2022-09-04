78bc1b56b6772fee9d6cfdea3ea95a37c13a5432
Krister Kittelson <krister@kittelson.us>
2022-09-04 13:51:10 -0500

    Update pies-pricesheetes and add pies-marketingcopy
    PriceSheets and MarketingCopy will be granulated at the top level -- a single element blob-like.

--- sandpiper_db.sql
+++ sandpiper_db.sql
@@ -333,7 +333,8 @@ INSERT INTO slice_types (slice_type) VALUES
 	, ('aces-app-elements'), ('aces-asset-elements')
 	, ('aces-digitalfileinfo-elements')
 	, ('partspro-file'), ('napa-interchange-file')
-	, ('pies-file'), ('pies-item-elements'), ('pies-pricesheet-elements')
+	, ('pies-file'), ('pies-item-elements')
+	, ('pies-pricesheets'), ('pies-marketingcopy')
 	, ('asset-file'), ('asset-archive'), ('asset-files')
 	, ('binary-blob'), ('xml-file'), ('text-file');
 
